d753b6cb5ac9b30bb565f43eda353faa9972814b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 13:04:56 +0530

    Create SampleWindow.java

---
+++ SampleWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class SampleWindow {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My Window");
+        myWindow.setVisible(true);
+    }
+}
