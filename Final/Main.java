298b15197269f1a5ae9722fe3b7ea312a4136944
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 15:25:50 +0530

    Create Main.java

---
+++ Final/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int X = 10;
+        System.out.println(X);
+//        X = 20;                                   will generate an error
+//        System.out.println(X);
+    }
+}
