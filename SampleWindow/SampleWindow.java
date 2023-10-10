b82c489a739cbf1f3187ed601bc1fe446e85d19f
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 13:34:18 +0530

    Update SampleWindow.java

--- SampleWindow/SampleWindow.java
+++ SampleWindow/SampleWindow.java
@@ -3,7 +3,7 @@ class SampleWindow {
     public static void main(String[] args) {
         JFrame myWindow;
         myWindow = new JFrame();
-        myWindow.setSize(300, 200);
+        myWindow.setSize(300, 200);  // Can change the size of the window
         myWindow.setTitle("My Window");
         myWindow.setVisible(true);  // myWindow.setVisible(false);-- Can't see the window
     }
