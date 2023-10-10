5a7f0335c0799d29e94309eb734eb326b31d8228
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 15:29:40 +0530

    Update Main.java

--- Variables/Main.java
+++ Variables/Main.java
@@ -2,9 +2,20 @@ public class Main {
   public static void main(String[] args) {
     String name = "John";
     int X = 10;
+    
     System.out.println(name);
     System.out.println(X);
+    
     X = 20;
+    
     System.out.println(X);
+    
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
   }
 }
