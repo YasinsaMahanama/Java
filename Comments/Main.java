0293159848e4b878614d48c18f7667bd03ef9324
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 23:23:39 +0530

    Create Main.java

---
+++ Comments/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+
+    //This is a single line comment
+        System.out.println("Hello World");
+    }
+}
