378befc95203abfbd37afae1c7909c3dc230fc1a
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-10 16:12:00 +0530

    Update Main.java

--- Print Text/Main.java
+++ Print Text/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.print("I am Yasinsa.\n");
     System.out.println("I am learning Java.");
     System.out.println("It is awesome!");                //in must be wrapped iside double quotations marks ""
   }
