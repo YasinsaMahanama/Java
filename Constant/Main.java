5ea2f424a211053d8e0171f75b47f8fb323d56e7
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 09:15:54 +0530

    Create Main.java

---
+++ Constant/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+        final short FARADAY_CONSTANT = 23060;
+        final double CM_PER_INCH = 2.54;
+        final int MONTHS_IN_YEAR = 12;
+
+        System.out.println(PI);
+        System.out.println(FARADAY_CONSTANT);
+        System.out.println(CM_PER_INCH);
+        System.out.println(MONTHS_IN_YEAR);
+
+    }
+
+}
