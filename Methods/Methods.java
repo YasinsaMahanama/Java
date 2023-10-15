bfc0c653b3469ec53443867b567a41615d46200a
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 17:41:23 +0530

    Create Methods.java

---
+++ Methods/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        int res = add();
+        res = res*10;
+        System.out.println(res);
+    }
+    public static int add(){
+        int a =15;
+        int b = 5;
+        int c = a+b;
+        return c;
+    }
+}
