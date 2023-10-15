c66d992e7b2c4e11fa5ce06640fc74c61a3fbb42
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 10:13:39 +0530

    Create Main.java

---
+++ Standard Output/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I Love Java");
+
+        System.out.println("Shopping List:");
+        System.out.println("\tApple");
+        System.out.println("\tBanana");
+        System.out.println("\tLow-fat Milk");
+
+    }
+
+}
