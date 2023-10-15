9721026d969d3bb9638f072e21f62c9d67dc215f
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 10:36:36 +0530

    Create Main.java

---
+++ Substring/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String country;
+        country = "Sri Lanka";
+        System.out.print(country.substring(0, 8));
+    }
+
+}
