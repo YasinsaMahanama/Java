0601a44f11e85dbf355e0d9ec65b99ff8ceb83c9
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 10:22:44 +0530

    Create Main.java

---
+++ Declare Multiple Variables/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int z = 9;
+
+        System.out.println(x + y + z);
+
+    }
+
+}
