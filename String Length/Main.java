87872ecef925839f8d121370e3ee9bc514508c68
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 10:39:20 +0530

    Create Main.java

---
+++ String Length/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String country;
+        country = "Sri Lanka";
+        System.out.println(country.substring(0, 8));
+        System.out.println(country.length());
+    }
+
+}
