d19fe8771a9fd0cdbe31266c149296dcf6c1e4c9
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-15 17:49:18 +0530

    Create Methods.java

---
+++ Methods/No parameters, No return Type/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        add();
+
+    }
+    public static void add(){
+        int a =15;
+        int b = 5;
+        int c = a+b;
+
+        System.out.println(c);
+    }
+}
