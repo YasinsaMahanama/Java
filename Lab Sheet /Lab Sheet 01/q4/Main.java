5e8d2f8953e246205ec4a37db618a8ab9154b745
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-24 14:44:44 +0530

    Create Main.java

---
+++ Lab Sheet /Lab Sheet 01/q4/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class SampleWindow {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My First Frame");
+        myWindow.setVisible(true);
+        myWindow.setLocation(50, 50);
+    }
+}
