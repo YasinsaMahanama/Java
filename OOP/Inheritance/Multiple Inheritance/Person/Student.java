028cff7fadcb48057c024b3d83996cd2d854b51b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-04 22:46:12 +0530

    Create Student.java

---
+++ OOP/Inheritance/Multiple Inheritance/Person/Student.java
@@ -0,0 +1,12 @@
+class Student extends Person {
+    private String studentId;
+
+    public Student(String name, int age, String studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+}
