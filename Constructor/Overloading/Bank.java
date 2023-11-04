9f0b1ba157fc3c936dab4593e21fe1592b6c1e85
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-04 18:32:59 +0530

    Create Bank.java

---
+++ Constructor/Overloading/Bank.java
@@ -0,0 +1,18 @@
+public class Bank {
+    int rupees;
+    Bank(){
+        rupees = 0;
+    }
+
+    Bank(int x){
+        rupees = x;
+    }
+
+    public static void main(String[] args) {
+        Bank kamal = new Bank();
+        Bank nimal = new Bank(20);
+
+        System.out.println(kamal.rupees);
+        System.out.println(nimal.rupees);
+    }
+}
