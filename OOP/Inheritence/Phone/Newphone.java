0ae37bf2d639da637ccb4339bb73dfd447be7134
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-04 21:16:23 +0530

    Create Newphone.java

---
+++ OOP/Inheritence/Phone/Newphone.java
@@ -0,0 +1,8 @@
+public class Newphone extends Oldphone{
+    public static void main(String[] args) {
+        Newphone obj2 = new Newphone();
+        obj2.name = "Mahanama";
+        obj2.call();
+        obj2.message();
+    }
+}
