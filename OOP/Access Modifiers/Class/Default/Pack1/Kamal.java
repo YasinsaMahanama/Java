f696d0072f4cbc9e8d22095cadb87ccdde085742
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 09:30:41 +0530

    Create Kamal.java

---
+++ OOP/Access Modifiers/Class/Default/Pack1/Kamal.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+import Pack2.Saman;
+
+public class Kamal {
+    public static void main(String[] args) {
+        Nimal obj1 = new Nimal();
+        //Saman obj2 = new Saman();    can't write this,because Saman is default class
+    }
+}
