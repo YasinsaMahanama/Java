23d9eeb589e249103dc790018a9953791c963305
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:11:55 +0530

    Create Saman.java

---
+++ OOP/Access Modifiers/Methods-Variables/Default/Pack2/Saman.java
@@ -0,0 +1,9 @@
+package Pack2;
+import Pack1.*;
+public class Saman {
+    public static void main(String[] args) {
+        Kamal obj3 = new Kamal();
+//        obj3.x = 20;
+//        System.out.println(obj3.x);      can't write this, because x is a default variable
+    }
+}
