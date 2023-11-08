7e5defbff9d6af6ee6e56d1e8b7788a66b48950c
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:18:15 +0530

    Create Saman.java

---
+++ OOP/Access Modifiers/Methods-Variables/Protected/Pack2/Saman.java
@@ -0,0 +1,9 @@
+package Pack2;
+import Pack1.*;
+public class Saman extends Kamal {
+    public static void main(String[] args) {
+        Saman obj3 = new Saman();
+        obj3.x = 20;
+        System.out.println(obj3.x);
+    }
+}
