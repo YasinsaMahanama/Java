f88ee47ab959e4cd90491df89defb6ad6addb2db
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:17:14 +0530

    Create Kamak.java

---
+++ OOP/Access Modifiers/Methods-Variables/Protected/Pack1/Kamak.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+public class Kamal {
+    protected int x;
+
+    public static void main(String[] args) {
+        Kamal obj1 = new Kamal();
+        obj1.x = 10;
+        System.out.println(obj1.x);
+    }
+}
