4c50ab0f6048ba8951a4fc7d9ab193da798f160b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:06:33 +0530

    Create Kamal.java

---
+++ OOP/Access Modifiers/Methods-Variables/Public/Pack1/Kamal.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+public class Kamal {
+    public int x;
+
+    public static void main(String[] args) {
+        Kamal ob1 = new Kamal();
+        ob1.x = 10;
+        System.out.println(ob1.x);
+    }
+}
