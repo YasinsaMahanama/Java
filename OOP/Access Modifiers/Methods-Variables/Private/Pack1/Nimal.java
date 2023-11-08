5ac05a907f94cf93964755a5d1478c3dccc2f6b0
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:22:37 +0530

    Update Nimal.java

--- OOP/Access Modifiers/Methods-Variables/Private/Pack1/Nimal.java
+++ OOP/Access Modifiers/Methods-Variables/Private/Pack1/Nimal.java
@@ -4,6 +4,6 @@ public class Nimal {
     public static void main(String[] args) {
         Kamal obj2 = new Kamal();
 //        obj2.x = 15;
-//        System.out.println(obj2.x);
+//        System.out.println(obj2.x);              can't write this, because x is a private variable
     }
 }
