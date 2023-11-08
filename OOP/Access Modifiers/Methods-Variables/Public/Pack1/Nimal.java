7b09c1f9242a711a8f851a0b48074d106a163b8a
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 10:06:52 +0530

    Create Nimal.java

---
+++ OOP/Access Modifiers/Methods-Variables/Public/Pack1/Nimal.java
@@ -0,0 +1,9 @@
+package Pack1;
+
+public class Nimal {
+    public static void main(String[] args) {
+        Kamal obj2 = new Kamal();
+        obj2.x = 15;
+        System.out.println(obj2.x);
+    }
+}
