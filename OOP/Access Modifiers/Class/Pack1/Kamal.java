b4654281ee849796b794af364da1e91dc7071e1e
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 09:26:08 +0530

    Create Kamal.java

---
+++ OOP/Access Modifiers/Class/Pack1/Kamal.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+import Pack2.Saman;
+
+public class Kamal {
+    public static void main(String[] args) {
+        Nimal obj1 = new Nimal();
+        Saman obj2 = new Saman();
+    }
+}
