f39f51f93d4654267451b1dc2607cd2e1a69c6dc
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-08 09:32:06 +0530

    Update Kamal.java

--- OOP/Access Modifiers/Class/Default/Pack1/Kamal.java
+++ OOP/Access Modifiers/Class/Default/Pack1/Kamal.java
@@ -5,6 +5,6 @@ import Pack2.Saman;
 public class Kamal {
     public static void main(String[] args) {
         Nimal obj1 = new Nimal();
-        //Saman obj2 = new Saman();    can't write this,because Saman is default class
+        //Saman obj2 = new Saman();    can't write this,because Saman is default class and it is in the another package
     }
 }
