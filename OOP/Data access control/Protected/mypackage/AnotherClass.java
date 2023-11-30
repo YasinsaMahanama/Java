e58d4c15d80c3a61591363e03aa227e2c305e733
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-30 17:18:53 +0530

    Create AnotherClass.java

---
+++ OOP/Data access control/Protected/mypackage/AnotherClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class AnotheClass {
+    public static void main(String args[]){
+        SubClass subobject = new SubClass();
+
+        subobject.accessProtectedMembers();
+    }
+}
