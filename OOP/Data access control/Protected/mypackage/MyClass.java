79df8eba52c8f7ec9aa800243d879ad38bf0a464
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-30 17:18:04 +0530

    Create MyClass.java

---
+++ OOP/Data access control/Protected/mypackage/MyClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class MyClass {
+    protected int protectedVariable;
+
+    protected void protectedMethod(){
+        System.out.println("This is a protected method.");
+    }
+}
